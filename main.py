559294d2624273b9ea2369549d5fffeb7de9d5f0
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2018-11-27 23:16:41 +0100

    main.py: add function for the simulation & create .gitignore file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from vehicle import Vehicle
 import numpy as np
+import matplotlib.animation as animation
+import matplotlib.patches as patches
 
 img=mpimg.imread('firstedit.png')
 
@@ -11,5 +13,38 @@ imgplot = ax.imshow(img)
 car = Vehicle()
 car.show_random_route(fig,ax)
 
-# to stop plot from closing
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
+
+v = np.array([
+    [0.1, 1],
+    [2, 1],
+    [2, 2]
+])
+
+patch = patches.Polygon(v, closed=None, fill=None, edgecolor='r')
+
+def init():
+    ax.add_patch(patch)
+    return patch,
+
+
+def animate(i):
+    pc = 0.1
+    path = patch.get_xy()
+    point1 = path[0]
+    point2 = path[1]
+    point3 = path[2]
+    ar = [[point1[0] + pc, point1[1] + pc], [point2[0] + pc, point2[1] + pc], [point3[0] + pc, point3[1] + pc]]
+
+    patch.set_xy(np.array(ar))
+    return patch,
+
+anim = animation.FuncAnimation(fig, animate,
+                               init_func=init,
+                               frames=360,
+                               interval=1000,
+                               blit=False)
 plt.show()
