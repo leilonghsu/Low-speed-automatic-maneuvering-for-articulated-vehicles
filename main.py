287b7487324a58a0ff04ffff3cf9a9af904453c0
Magnus Andersson <gusbannbma@student.gu.se>
2018-11-27 13:40:46 +0100

    Update to self made image

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from vehicle import Vehicle
+import numpy as np
 
-img=mpimg.imread('parking.png')
+img=mpimg.imread('firstedit.png')
+
 fig,ax = plt.subplots(1)
 imgplot = ax.imshow(img)
 
--- vehicle.py
+++ vehicle.py
@@ -1,5 +1,6 @@
 from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 class Vehicle:
     def __init__(self,fig,ax):
@@ -7,7 +8,14 @@ class Vehicle:
         self.ax = ax
         chassi = Rectangle((100,100), 24.4, 78.2, fill = False)
         self.ax.add_patch(chassi)
+        
+        t = mpl.transforms.Affine2D().rotate_deg(-25) + self.ax.transData
         for _ in range(10):
             chassi.set_xy(tuple(1.1*i for i in chassi.xy))
+            
+            
+
+            chassi.set_transform(t)
+
             plt.pause(0.1)
         print(chassi.xy)
