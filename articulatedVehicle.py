921cfad1cb2b5747769b7247bf8ae1ba5d2ebb3b
Magnus Andersson <gusbannbma@student.gu.se>
2018-12-03 10:35:09 +0100

    Add update movement t articulatedVehicle

--- articulatedVehicle.py
+++ articulatedVehicle.py
@@ -1,43 +1,95 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import math
+import numpy as np
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-xAxisStart = -10
-xAxisEnd = 10
-yAxisStart = -10
-yAxisEnd = 10
-ax.set_xlim(xAxisStart, xAxisEnd)
-ax.set_ylim(yAxisStart, yAxisEnd)
+class ArticulatedVehicle:
 
-trailorW = 2
-trailorH = 1
-headW = 0.5
-headH = 0.5
-startPointX = -5
-startPointY = -8
-lineW = 0.2
-lineH = 0.5
+    def __init__(self, fig, ax):
+        self.fig = fig
+        self.ax = ax
+        #self.xAxisStart = -10
+        #self.xAxisEnd = 10
+        #self.yAxisStart = -10
+        #self.yAxisEnd = 10
+        #self.ax.set_xlim(self.xAxisStart, self.xAxisEnd)
+        #self.ax.set_ylim(self.yAxisStart, self.yAxisEnd)
+        self.velocity = 0
+        self.trailerW = 80
+        self.trailerH = 24.4
+        self.headW = 35
+        self.headH = 20
+        self.startPointX = 15
+        self.startPointY = 20
+        self.lineW = 10
+        self.lineH = 2
+        self.lineA = 0
+        self.headAngle = 0
+        self.trailerAngle = 0
 
-assert startPointX >= xAxisStart, "The start point is out of the x axis. Try to change the inputs"
-assert startPointY >= yAxisStart, "The start point is out of the y axis. Try to change the inputs"
+        #assert self.startPointX >= self.xAxisStart, "The start point is out of the x axis. Try to change the inputs"
+        #assert self.startPointY >= self.yAxisStart, "The start point is out of the y axis. Try to change the inputs"
 
-totalLength = trailorW + lineW + headW
-assert (totalLength + startPointX) < xAxisEnd, "The vehicle is out of the x axis. Try to change the inputs"
+        self.totalLength = self.trailerW + self.lineW + self.headW
+        #assert (self.totalLength + self.startPointX) < self.xAxisEnd, "The vehicle is out of the x axis. Try to change the inputs"
 
-totalHeight = trailorH
-assert (totalHeight + startPointY) < yAxisEnd, "The vehicle is out of the y axis. Try to change the inputs"
+        self.totalHeight = self.trailerH
+        #assert (self.totalHeight + self.startPointY) < self.yAxisEnd, "The vehicle is out of the y axis. Try to change the inputs"
 
-middleOfTrailor = (trailorH / 2) + startPointY
+        middleOfTrailer = (self.trailerH / 2) + self.startPointY
 
-trailor = plt.Rectangle((startPointX, startPointY), trailorW, trailorH, fc='r')
-plt.gca().add_patch(trailor)
+        self.trailer = plt.Rectangle((self.startPointX, self.startPointY), self.trailerW, self.trailerH, fc='r')
+        plt.gca().add_patch(self.trailer)
 
-line = plt.Line2D(((trailorW + startPointX), (trailorW + startPointX + lineW)), (middleOfTrailor, middleOfTrailor),
-                  lineH)
-plt.gca().add_line(line)
+        self.line = plt.Line2D(
+            ((self.trailerW + self.startPointX),
+             (self.trailerW + self.startPointX + self.lineW)),
+             (middleOfTrailer, middleOfTrailer),
+                        self.lineH)
+        plt.gca().add_line(self.line)
 
-yPositionForHead = (trailorH / 2) - (headH / 2) + startPointY
-rectangle2 = plt.Rectangle((trailorW + startPointX + lineW, yPositionForHead), headW, headH, fc='r')
-plt.gca().add_patch(rectangle2)
+        self.yPositionForHead = (self.trailerH / 2) - (self.headH / 2) + self.startPointY
+        self.head = plt.Rectangle((self.trailerW + self.startPointX + self.lineW, self.yPositionForHead), self.headW, self.headH, fc='r')
+        
+        plt.gca().add_patch(self.head)
+    
+    def move(self, vel, angle):
+        self.__updateHead(vel,angle)
+        self.__updateTrailer(angle)
 
-plt.show()
+        #print(self.head)
+
+    def __updateHead(self,vel,angle):
+        self.velocity += vel
+        self.headAngle += angle
+        _angle = np.deg2rad(self.headAngle)
+        (x,y) = self.head.xy
+        x1 = vel * math.cos(_angle)
+        y1 = vel * math.sin(_angle)
+        #print(x1,y1)
+        x += x1
+        y += y1
+        _x = x+(self.headH*0.5)
+        _y = y+(self.headW*0.5)
+        
+        t = mpl.transforms.Affine2D().rotate_around(_x,_y,_angle) + plt.gca().transData
+        self.head.set_transform(t)
+        self.head.set_xy((x,y))
+
+    def __updateTrailer(self,g):
+        (x1,y1) = self.head.xy
+        _angle = np.deg2rad(self.headAngle)
+        gamma = 180 - abs(abs(self.headAngle - self.trailerAngle) - 180)
+        angleChange = (self.velocity * math.sin(gamma) - self.headW * g * math.cos(gamma))/(self.headW * math.cos(gamma) + self.trailerW)
+        print(angleChange)
+
+        self.trailerAngle += angleChange
+        x2 = x1 - self.headW * math.cos(_angle) - self.trailerW * math.cos(_angle)
+        y2 = y1 - self.headW * math.sin(_angle) - self.trailerW * math.sin(_angle)
+
+        _x = x2 + (self.trailerH * 0.5)
+        _y = y2
+
+        t = mpl.transforms.Affine2D().rotate_around(_x,_y,_angle) + plt.gca().transData
+        self.trailer.set_transform(t)
+        self.trailer.set_xy((x2,y2))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from vehicle import Vehicle
+from articulatedVehicle import ArticulatedVehicle
 import numpy as np
 import matplotlib.animation as animation
 import matplotlib.patches as patches
@@ -9,42 +10,60 @@ img=mpimg.imread('firstedit.png')
 
 fig,ax = plt.subplots(1)
 imgplot = ax.imshow(img)
-
-car = Vehicle()
-car.show_random_route(fig,ax)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
-
-v = np.array([
-    [0.1, 1],
-    [2, 1],
-    [2, 2]
-])
-
-patch = patches.Polygon(v, closed=None, fill=None, edgecolor='r')
-
-def init():
-    ax.add_patch(patch)
-    return patch,
+av = ArticulatedVehicle(fig,ax)
 
 
-def animate(i):
-    pc = 0.1
-    path = patch.get_xy()
-    point1 = path[0]
-    point2 = path[1]
-    point3 = path[2]
-    ar = [[point1[0] + pc, point1[1] + pc], [point2[0] + pc, point2[1] + pc], [point3[0] + pc, point3[1] + pc]]
+def input_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print('That is not a valid number.')
 
-    patch.set_xy(np.array(ar))
-    return patch,
+while(True):
+    vel = input_float(">>>")
+    angle = input_float(">>>")
+    av.move(vel,angle)
+    plt.pause(1)
 
-anim = animation.FuncAnimation(fig, animate,
-                               init_func=init,
-                               frames=360,
-                               interval=1000,
-                               blit=False)
-plt.show()
+
+#
+#car = Vehicle()
+#car.show_random_route(fig,ax)
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111)
+#ax.set_xlim(0, 10)
+#ax.set_ylim(0, 10)
+#
+#v = np.array([
+#    [0.1, 1],
+#    [2, 1],
+#    [2, 2]
+#])
+#
+#patch = patches.Polygon(v, closed=None, fill=None, edgecolor='r')
+#
+#def init():
+#    ax.add_patch(patch)
+#    return patch,
+#
+#
+#def animate(i):
+#    pc = 0.1
+#    path = patch.get_xy()
+#    point1 = path[0]
+#    point2 = path[1]
+#    point3 = path[2]
+#    ar = [[point1[0] + pc, point1[1] + pc], [point2[0] + pc, point2[1] + pc], [point3[0] + pc, point3[1] + pc]]
+#
+#    patch.set_xy(np.array(ar))
+#    return patch,
+#
+#anim = animation.FuncAnimation(fig, animate,
+#                               init_func=init,
+#                               frames=360,
+#                               interval=1000,
+#                               blit=False)
+#plt.show()
+#
