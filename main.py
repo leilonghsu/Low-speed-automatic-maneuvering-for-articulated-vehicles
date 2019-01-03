41d0b89772fc4cb6d7305fe986d74d328d1f4a11
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2019-01-03 12:23:44 +0200

    convert probabilistic_roadmap to class and call it from main.py

--- main.py
+++ main.py
@@ -5,47 +5,81 @@ import numpy as np
 import matplotlib.animation as animation
 import matplotlib.patches as patches
 import time
+from probabilistic_roadmap import PRM
 
-img=mpimg.imread('firstedit.png')
+img = mpimg.imread('firstedit.png')
 
-fig,ax = plt.subplots(1)
+fig, ax = plt.subplots(1)
 imgplot = ax.imshow(img)
-av = ArticulatedVehicle(fig,ax)
+av = ArticulatedVehicle(fig, ax)
+
+
+def draw_plot(sx, sy, gx, gy, ox, oy):
+    plt.plot(ox, oy, ".k")
+    plt.plot(sx, sy, "^r")
+    plt.plot(gx, gy, "^c")
+    plt.grid(True)
+    plt.axis("equal")
+
+
+def start_prm():
+    sx = 1
+    sy = 1
+    gx = 9
+    gy = 4
+    robot_size = 5
+    ox = [1, 1, 1, 4, 4, 8, 8, 8]
+    oy = [3, 4, 5, 1, 2, 4, 5, 6]
+    draw_plot(sx, sy, gx, gy, ox, oy)
+
+    prm = PRM(sx, sy, gx, gy, robot_size, ox, oy)
+
+    rx, ry = prm.find_path()
+
+    print("PRM: path ")
+    print(rx, ry)
+
+    plt.plot(rx, ry, "-r")
+    plt.show()
 
 
 def input_float(prompt):
     while True:
         try:
             x = input(prompt)
-            if x == "": 
+            if x == "":
                 return 0
             return float(x)
         except ValueError:
             print('That is not a valid number.')
 
+
 previous_t = time.time()
 now = time.time()
-dt = now - previous_t 
+dt = now - previous_t
 previous_t = now
-av.move(20,1,dt)
-#526
+av.move(20, 1, dt)
+# 526
 i = 0
-while(i < 400):
-    i+=1
+while (i < 400):
+    i += 1
     if i < 60:
-        vel = 0#input_float(">>>")
-        angle = -i#input_float(">>>")
+        vel = 0  # input_float(">>>")
+        angle = -i  # input_float(">>>")
     elif i >= 60 and i < 120:
         vel = 0
-        angle = i-60
+        angle = i - 60
     else:
         vel = 0
         angle = 60
     now = time.time()
-    dt = now - previous_t 
+    dt = now - previous_t
     previous_t = now
-    av.move(vel,angle,0.1)
+    av.move(vel, angle, 0.1)
     plt.pause(.0001)
 plt.plot(av.save_hx, av.save_hy, "-r")
 plt.plot(av.save_tx, av.save_ty, "-b")
 plt.show()
+
+start_prm()
+
--- probabilistic_roadmap.py
+++ probabilistic_roadmap.py
@@ -2,7 +2,6 @@ import random
 import math
 import numpy as np
 import scipy.spatial
-import matplotlib.pyplot as plt
 
 N_SAMPLE = 500
 N_KNN = 10
@@ -29,126 +28,110 @@ class KDTree:
             return index, dist
 
 
-def is_collision(sx, sy, gx, gy, rr, obstacles_tree):
-    x = sx
-    y = sy
-    dx = gx - sx
-    dy = gy - sy
-    yaw = math.atan2(gy - sy, gx - sx)
-    d = math.sqrt(dx**2 + dy**2)
+class PRM:
+    def __init__(self, sx, sy, gx, gy, robot_size, ox, oy):
+        self.sx = sx
+        self.sy = sy
+        self.gx = gx
+        self.gy = gy
+        self.robot_size = robot_size
+        self.ox = ox
+        self.oy = oy
 
-    if d >= MAX_EDGE_LEN:
-        return True
+    def find_path(self):
+        rx, ry = self.construction_phase()
 
-    D = rr
-    nstep = round(d / D)
+        return rx, ry
 
-    for i in range(nstep):
-        idxs, dist = obstacles_tree.search(np.array([x, y]).reshape(2, 1))
+    def is_collision(self, sx, sy, gx, gy, rr, okdtree):
+        x = sx
+        y = sy
+        dx = gx - sx
+        dy = gy - sy
+        yaw = math.atan2(gy - sy, gx - sx)
+        d = math.sqrt(dx ** 2 + dy ** 2)
+
+        if d >= MAX_EDGE_LEN:
+            return True
+
+        D = rr
+        nstep = round(d / D)
+
+        for i in range(nstep):
+            idxs, dist = okdtree.search(np.array([x, y]).reshape(2, 1))
+            if dist[0] <= rr:
+                return True
+            x += D * math.cos(yaw)
+            y += D * math.sin(yaw)
+
+        idxs, dist = okdtree.search(np.array([gx, gy]).reshape(2, 1))
         if dist[0] <= rr:
             return True
-        x += D * math.cos(yaw)
-        y += D * math.sin(yaw)
 
-    idxs, dist = obstacles_tree.search(np.array([gx, gy]).reshape(2, 1))
-    if dist[0] <= rr:
+        return False
+
+    def generate_roadmap(self, sample_x, sample_y, obstacles_tree):
+        road_map = []
+        nsample = len(sample_x)
+        skdtree = KDTree(np.vstack((sample_x, sample_y)).T)
+
+        for (i, ix, iy) in zip(range(nsample), sample_x, sample_y):
+
+            index, dists = skdtree.search(
+                np.array([ix, iy]).reshape(2, 1), k=nsample)
+            inds = index[0]
+            edge_id = []
+
+            for ii in range(1, len(inds)):
+                nx = sample_x[inds[ii]]
+                ny = sample_y[inds[ii]]
+
+                if not self.is_collision(ix, iy, nx, ny, self.robot_size, obstacles_tree):
+                    edge_id.append(inds[ii])
+
+                if len(edge_id) >= N_KNN:
+                    break
+
+            road_map.append(edge_id)
+
+        return road_map
+
+    def random_points(self, obstacles_tree):
+        maxx = max(self.ox)
+        maxy = max(self.oy)
+        minx = min(self.ox)
+        miny = min(self.oy)
+
+        sample_x, sample_y = [], []
+
+        while len(sample_x) <= N_SAMPLE:
+            tx = (random.random() - minx) * (maxx - minx)
+            ty = (random.random() - miny) * (maxy - miny)
+
+            index, dist = obstacles_tree.search(np.array([tx, ty]).reshape(2, 1))
+
+            if dist[0] >= self.robot_size:
+                sample_x.append(tx)
+                sample_y.append(ty)
+
+        sample_x.append(self.sx)
+        sample_y.append(self.sy)
+        sample_x.append(self.gx)
+        sample_y.append(self.gy)
+
+        return sample_x, sample_y
+
+    def dijkstra_phase(self, road_map, sample_x, sample_y):
+        # TODO
         return True
 
-    return False
+    def construction_phase(self):
+        obstacles_tree = KDTree(np.vstack((self.ox, self.oy)).T)
 
+        sample_x, sample_y = self.random_points(obstacles_tree)
 
-def generate_roadmap(sample_x, sample_y, rr, obstacles_tree):
-    road_map = []
-    nsample = len(sample_x)
-    skdtree = KDTree(np.vstack((sample_x, sample_y)).T)
+        road_map = self.generate_roadmap(sample_x, sample_y, obstacles_tree)
 
-    for (i, ix, iy) in zip(range(nsample), sample_x, sample_y):
+        rx, ry = self.dijkstra_phase(road_map, sample_x, sample_y)
 
-        index, dists = skdtree.search(
-            np.array([ix, iy]).reshape(2, 1), k=nsample)
-        inds = index[0]
-        edge_id = []
-
-        for ii in range(1, len(inds)):
-            nx = sample_x[inds[ii]]
-            ny = sample_y[inds[ii]]
-
-            if not is_collision(ix, iy, nx, ny, rr, obstacles_tree):
-                edge_id.append(inds[ii])
-
-            if len(edge_id) >= N_KNN:
-                break
-
-        road_map.append(edge_id)
-
-    return road_map
-
-
-def random_points(sx, sy, gx, gy, rr, ox, oy, obstacles_tree):
-    maxx = max(ox)
-    maxy = max(oy)
-    minx = min(ox)
-    miny = min(oy)
-
-    sample_x, sample_y = [], []
-
-    while len(sample_x) <= N_SAMPLE:
-        tx = (random.random() - minx) * (maxx - minx)
-        ty = (random.random() - miny) * (maxy - miny)
-
-        index, dist = obstacles_tree.search(np.array([tx, ty]).reshape(2, 1))
-
-        if dist[0] >= rr:
-            sample_x.append(tx)
-            sample_y.append(ty)
-
-    sample_x.append(sx)
-    sample_y.append(sy)
-    sample_x.append(gx)
-    sample_y.append(gy)
-
-    return sample_x, sample_y
-
-
-def dijkstra_phase(sx, sy, gx, gy, ox, oy, rr, road_map, sample_x, sample_y):
-    #TODO
-    return True
-
-
-def construction_phase(sx, sy, gx, gy, ox, oy, robot_size):
-    obstacles_tree = KDTree(np.vstack((ox, oy)).T)
-
-    sample_x, sample_y = random_points(sx, sy, gx, gy, robot_size, ox, oy, obstacles_tree)
-
-    plt.plot(sample_x, sample_y, ".b")
-
-    road_map = generate_roadmap(sample_x, sample_y, robot_size, obstacles_tree)
-
-    rx, ry = dijkstra_phase(sx, sy, gx, gy, ox, oy, robot_size, road_map, sample_x, sample_y)
-
-    return rx, ry
-
-
-def main():
-    sx = 1
-    sy = 1
-    gx = 9
-    gy = 4
-    robot_size = 5.0  # TODO
-
-    ox = [1, 1, 1, 4, 4, 8, 8, 8]
-    oy = [3, 4, 5, 1, 2, 4, 5, 6]
-
-    plt.plot(ox, oy, ".k")
-    plt.plot(sx, sy, "^r")
-    plt.plot(gx, gy, "^c")
-    plt.grid(True)
-    plt.axis("equal")
-
-    rx, ry = construction_phase(sx, sy, gx, gy, ox, oy, robot_size)
-
-    plt.plot(rx, ry, "-r")
-    plt.show()
-
-
-main()
+        return rx, ry
