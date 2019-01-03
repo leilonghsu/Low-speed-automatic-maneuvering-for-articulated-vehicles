234e4db6710182c8280a81f87bf60e54c26ca223
Paniskaki Kyriaki <kiki_paniskaki@hotmail.com>
2019-01-03 23:50:28 +0200

    some changes for articulated vehicle, use one isntance of the plot, still working on moving vehicle on the path

--- articulatedVehicle.py
+++ articulatedVehicle.py
@@ -1,36 +1,35 @@
-import matplotlib.pyplot as plt
 import math
 import numpy as np
 import matplotlib.patches as patches
-import time
 
 
 class ArticulatedVehicle:
 
-    def __init__(self, fig, ax):
-        self.fig = fig
-        self.ax = ax
+    def __init__(self, plt):
+        self.plt = plt
         self.velocity = 0
         self.vx = 0
         self.vy = 0
-        self.trailerW = 65
-        trailerH = 26
-        self.headW = 30
-        headH = 26
-        self.startPointX = 155
-        self.startPointY = 213
+        self.trailerW = 6
+        trailerH = 2
+        self.headW = 3
+        headH = 2
+        self.startPointX = 10
+        self.startPointY = 10
         self.headAngle = 0
         self.trailerAngle = 0
         self.max_v = 2
         self.max_th = 1
         self.save_hx = [self.startPointX]
         self.save_hy = [self.startPointY]
-        self.save_tx = [self.startPointX - (self.headW/2 * math.cos(np.radians(self.headAngle))) - (self.trailerW * math.cos(np.radians(self.trailerAngle)))]
-        self.save_ty = [self.startPointY - (self.headW/2 * math.sin(np.radians(self.headAngle))) - (self.trailerW * math.sin(np.radians(self.trailerAngle)))]
-        
+        self.save_tx = [self.startPointX - (self.headW / 2 * math.cos(np.radians(self.headAngle))) - (
+                self.trailerW * math.cos(np.radians(self.trailerAngle)))]
+        self.save_ty = [self.startPointY - (self.headW / 2 * math.sin(np.radians(self.headAngle))) - (
+                self.trailerW * math.sin(np.radians(self.trailerAngle)))]
+
         hW = self.headW / 2
         hH = headH / 2
-        tW = self.trailerW 
+        tW = self.trailerW
         tH = trailerH / 2
 
         p0 = [self.startPointX + hW, self.startPointY - hH]
@@ -40,27 +39,25 @@ class ArticulatedVehicle:
         p4 = [self.startPointX - hW, self.startPointY - tH]
         p5 = [self.startPointX - hW - tW, self.startPointY - tH]
         p6 = [self.startPointX - hW - tW, self.startPointY + tH]
-        p7 = [self.startPointX  - hW, self.startPointY + tH] 
+        p7 = [self.startPointX - hW, self.startPointY + tH]
 
-        headVectors = np.array([p0,p1,p2,p3])
-        trailerVectors = ([p4,p5,p6,p7])
+        headVectors = np.array([p0, p1, p2, p3])
+        trailerVectors = ([p4, p5, p6, p7])
         self.truckHead = patches.Polygon(headVectors, fill=None)
         self.truckTrailer = patches.Polygon(trailerVectors, fill=None)
-        ax.add_patch(self.truckHead)
-        ax.add_patch(self.truckTrailer)
+        self.plt.gca().add_patch(self.truckHead)
+        self.plt.gca().add_patch(self.truckTrailer)
 
-    
     def move(self, vel, angle, dt):
-        self.__updateHead(vel,angle, dt)
+        self.__updateHead(vel, angle, dt)
 
-    def __updateHead(self,vel,angle, dt):
+    def __updateHead(self, vel, angle, dt):
         self.velocity += vel
 
-        angleChange = self.velocity/self.headW * math.tan(np.radians(angle))
+        angleChange = self.velocity / self.headW * math.tan(np.radians(angle))
         self.headAngle += angleChange
-        
+
         theta = np.radians(angleChange)
-        
 
         oldX = self.startPointX
         oldY = self.startPointY
@@ -74,59 +71,57 @@ class ArticulatedVehicle:
         #     o------------o
         self.startPointX += (dt * self.velocity) * math.cos(np.radians(self.headAngle))
         self.startPointY += (dt * self.velocity) * math.sin(np.radians(self.headAngle))
-        
+
         self.save_hx.append(self.startPointX)
         self.save_hy.append(self.startPointY)
-        
 
         points = self.truckHead.get_xy()
-        [x,y] = points[0]
+        [x, y] = points[0]
 
         _x = self.startPointX + (math.cos(theta) * (x - oldX)) - (math.sin(theta) * (y - oldY))
         _y = self.startPointY + (math.sin(theta) * (x - oldX)) + (math.cos(theta) * (y - oldY))
 
-        t = np.array([_x,_y])
+        t = np.array([_x, _y])
         newPoints = np.array([t])
 
-        for i in range(1,len(points)):
-            [x,y] = points[i]
+        for i in range(1, len(points)):
+            [x, y] = points[i]
             _x = self.startPointX + (math.cos(theta) * (x - oldX)) - (math.sin(theta) * (y - oldY))
             _y = self.startPointY + (math.sin(theta) * (x - oldX)) + (math.cos(theta) * (y - oldY))
-            t = np.array([_x,_y])
+            t = np.array([_x, _y])
             newPoints = np.append(newPoints, [t], axis=0)
 
         self.truckHead.set_xy(newPoints)
-        self.__updateTrailer(angleChange,oldX,oldY,dt)
+        self.__updateTrailer(angleChange, oldX, oldY, dt)
 
-    def __updateTrailer(self,th,oldX,oldY,dt):
+    def __updateTrailer(self, th, oldX, oldY, dt):
         v = self.truckTrailer.get_xy()
         gamma = np.radians(self.headAngle - self.trailerAngle)
-        
-        angleChange = -th * ((1/self.trailerW)*math.cos(gamma) + 1) - ((self.velocity/self.trailerW) * math.sin(gamma))
+
+        angleChange = -th * ((1 / self.trailerW) * math.cos(gamma) + 1) - (
+                (self.velocity / self.trailerW) * math.sin(gamma))
 
         self.trailerAngle += angleChange
         theta = -np.radians(angleChange)
 
-
-
         hTheta = np.radians(self.headAngle)
         tTheta = -np.radians(self.trailerAngle)
-        self.save_tx.append(self.startPointX - (self.headW/2 * math.cos(hTheta)) - (self.trailerW * math.cos(tTheta)))
-        self.save_ty.append(self.startPointY - (self.headW/2 * math.sin(hTheta)) - (self.trailerW * math.sin(tTheta)))
+        self.save_tx.append(self.startPointX - (self.headW / 2 * math.cos(hTheta)) - (self.trailerW * math.cos(tTheta)))
+        self.save_ty.append(self.startPointY - (self.headW / 2 * math.sin(hTheta)) - (self.trailerW * math.sin(tTheta)))
 
-        [x,y] = v[0]
+        [x, y] = v[0]
         _x = self.startPointX + math.cos(theta) * (x - oldX) - math.sin(theta) * (y - oldY)
         _y = self.startPointY + math.sin(theta) * (x - oldX) + math.cos(theta) * (y - oldY)
-        
-        t = np.array([_x,_y])
+
+        t = np.array([_x, _y])
         newV = np.array([t])
 
-        for i in range(1,len(v)):
-            [x,y] = v[i]
+        for i in range(1, len(v)):
+            [x, y] = v[i]
 
             _x = self.startPointX + math.cos(theta) * (x - oldX) - math.sin(theta) * (y - oldY)
             _y = self.startPointY + math.sin(theta) * (x - oldX) + math.cos(theta) * (y - oldY)
-            t = np.array([_x,_y])
+            t = np.array([_x, _y])
             newV = np.append(newV, [t], axis=0)
 
         self.truckTrailer.set_xy(newV)
@@ -176,6 +171,9 @@ class ArticulatedVehicle:
                     angle = 9
                     vel = 6
                     turn_movements += 1
+
+            self.startPointX = rx[i]
+            self.startPointY = ry[i]
 
             self.move(vel, angle, 0.1)
 
--- main.py
+++ main.py
@@ -1,85 +1,60 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from articulatedVehicle import ArticulatedVehicle
-import numpy as np
-import matplotlib.animation as animation
-import matplotlib.patches as patches
-import time
 from probabilistic_roadmap import PRM
 
-img = mpimg.imread('firstedit.png')
 
-fig, ax = plt.subplots(1)
-imgplot = ax.imshow(img)
-av = ArticulatedVehicle(fig, ax)
+def start_prm():
+    # sx = 1
+    # sy = 1
+    # gx = 9
+    # gy = 4
+    # robot_size = 5
+    # ox = [1, 1, 1, 4, 4, 8, 8, 8]
+    # oy = [3, 4, 5, 1, 2, 4, 5, 6]
+    #draw_plot(sx, sy, gx, gy, ox, oy)
 
+    sx = 10.0  # [m]
+    sy = 10.0  # [m]
+    gx = 50.0  # [m]
+    gy = 50.0  # [m]
+    robot_size = 5.0  # [m]
 
-def draw_plot(sx, sy, gx, gy, ox, oy):
+    ox = []
+    oy = []
+
+    for i in range(60):
+        ox.append(i)
+        oy.append(0.0)
+    for i in range(60):
+        ox.append(60.0)
+        oy.append(i)
+    for i in range(61):
+        ox.append(i)
+        oy.append(60.0)
+    for i in range(61):
+        ox.append(0.0)
+        oy.append(i)
+    for i in range(40):
+        ox.append(20.0)
+        oy.append(i)
+    for i in range(40):
+        ox.append(40.0)
+        oy.append(60.0 - i)
+
     plt.plot(ox, oy, ".k")
     plt.plot(sx, sy, "^r")
     plt.plot(gx, gy, "^c")
     plt.grid(True)
-    plt.axis("equal")
-
-
-def start_prm():
-    sx = 1
-    sy = 1
-    gx = 9
-    gy = 4
-    robot_size = 5
-    ox = [1, 1, 1, 4, 4, 8, 8, 8]
-    oy = [3, 4, 5, 1, 2, 4, 5, 6]
-    draw_plot(sx, sy, gx, gy, ox, oy)
 
     prm = PRM(sx, sy, gx, gy, robot_size, ox, oy)
 
     rx, ry = prm.find_path()
 
-    print("PRM: path ")
-    print(rx, ry)
-
     plt.plot(rx, ry, "-r")
-    plt.show()
+    av = ArticulatedVehicle(plt)
     av.move_on_path(rx, ry)
 
+    plt.show()
 
-def input_float(prompt):
-    while True:
-        try:
-            x = input(prompt)
-            if x == "":
-                return 0
-            return float(x)
-        except ValueError:
-            print('That is not a valid number.')
-
-
-previous_t = time.time()
-now = time.time()
-dt = now - previous_t
-previous_t = now
-av.move(20, 1, dt)
-# 526
-i = 0
-while (i < 400):
-    i += 1
-    if i < 60:
-        vel = 0  # input_float(">>>")
-        angle = -i  # input_float(">>>")
-    elif i >= 60 and i < 120:
-        vel = 0
-        angle = i - 60
-    else:
-        vel = 0
-        angle = 60
-    now = time.time()
-    dt = now - previous_t
-    previous_t = now
-    av.move(vel, angle, 0.1)
-    plt.pause(.0001)
-plt.plot(av.save_hx, av.save_hy, "-r")
-plt.plot(av.save_tx, av.save_ty, "-b")
-plt.show()
 
 start_prm()
